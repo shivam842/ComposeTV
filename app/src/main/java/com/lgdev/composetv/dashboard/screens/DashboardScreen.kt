4e42e4bcffdfa01b1bb8d4fdadb79f7df5c3b886
DESKTOP-5OC8TEQ\Bhomkar <sagar.bhomkar@gmail.com>
2024-01-15 13:23:34 +0530

    Added row for tap and tranding movie

--- app/src/main/java/com/lgdev/composetv/dashboard/screens/DashboardScreen.kt
+++ app/src/main/java/com/lgdev/composetv/dashboard/screens/DashboardScreen.kt
@@ -3,13 +3,17 @@
 package com.lgdev.composetv.dashboard.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.tv.foundation.lazy.list.TvLazyRow
 import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.Text
 
 @Composable
 fun DashboardScreen(modifier: Modifier = Modifier) {
@@ -39,12 +43,25 @@ fun BannerView(modifier: Modifier = Modifier) {
 
 @Composable
 fun TrendingListView(modifier: Modifier = Modifier) {
-
+    val list:List<Item> = mutableListOf()
+    Column(
+        modifier = modifier
+            .wrapContentHeight()
+            .fillMaxWidth()) {
+        Text(text = " Trending Movies")
+        MovieList(list)
+    }
 
 }
 
 @Composable
 fun TopListView(modifier: Modifier = Modifier) {
-
-
+    val list:List<Item> = mutableListOf()
+    Column(
+        modifier = modifier
+            .wrapContentHeight()
+            .fillMaxWidth()) {
+        Text(text = "Today's top Movies")
+        MovieList(list)
+    }
 }
